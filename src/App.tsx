41cbcdcff806d672fdc480f0df370317b82fab0f
NeuralLark <vyralcat@gmail.com>
2025-12-31 00:35:31 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
+import BaselineSwapPanel from "./components/BaselineSwapPanel";
 
 const queryClient = new QueryClient();
 
@@ -13,6 +14,10 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+
+      {/* ✅ Always visible: Wallet Connect + Swap + CA + Socials */}
+      <BaselineSwapPanel />
+
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
