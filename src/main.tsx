688f722db5613e5166aeea8335ee3442465c1600
NeuralLark <vyralcat@gmail.com>
2025-12-30 20:56:10 -0600

    Wrap App component with WalletAppProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+import WalletAppProvider from "./WalletAppProvider";
+import "@solana/wallet-adapter-react-ui/styles.css";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <WalletAppProvider>
+    <App />
+  </WalletAppProvider>
+);
